717a57685df5f11c9613d554fcbacb22f245ef31
Liku cs <83144124+likucs@users.noreply.github.com>
2022-04-12 01:09:19 +0530

    Update generate_screen_shot.py

--- plugins/generate_screen_shot.py
+++ plugins/generate_screen_shot.py
@@ -87,7 +87,7 @@ async def generate_screen_shot(bot, update):
             media_album_p = []
             if images is not None:
                 i = 0
-                caption = "Join : @TGBotsCollection \nFor the list of Telegram Bots"
+                caption = "Join : @cs_cloud \nFor the list of Telegram Bots"
                 for image in images:
                     if os.path.exists(image):
                         if i == 0:
